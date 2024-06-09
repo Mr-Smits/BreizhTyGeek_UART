2c7221faa1f63c446cbebbc0b6c25ad0eb869a7a
Oxbian <38785328+Oxbian@users.noreply.github.com>
2024-06-09 10:19:38 +0200

    Ajout gitignore, README, + lecture du son et multi platform sur le code (#1)

--- CODE.py
+++ CODE.py
@@ -1,22 +1,39 @@
 import os
+import platform
+import vlc
+import time
 
 mot_secret = "CODE"
 compteur = 0
+
+def play_sound(file):
+    player = vlc.MediaPlayer(file)
+    player.play()
+    time.sleep(1)  # Laissez le temps au lecteur de commencer
+    while player.is_playing():
+        time.sleep(0.1)
+    player.stop()
+
+def clear_screen():
+    if platform.system() == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
 
 while True:
     guess = input("Devinez le mot secret : ").upper()
     
     if guess == mot_secret:
         print("Félicitations ! Vous avez trouvé le mot secret.")
-        input()
-        os.system('cls')
+        play_sound("Victory.mp3")
+        clear_screen()
     else:
         if compteur == 0:
             print("Oops, ce n'est pas le mot secret. Essayez à nouveau !")
         elif compteur == 1:
             print("Hélas, ce n'est pas le mot secret. Mais ne te décourage pas !")
         elif compteur == 2:
-            print("Ah zut, ce n'est pas encore ça. Continue à essayer !")
+            print("Ah zut, ce n'est encore pas ça. Continue à essayer !")
         else:
             print("Dommage, ce n'est pas le mot secret. Peut-être as-tu besoin d'un déCODEur ? Essayez encore !")
         compteur += 1
